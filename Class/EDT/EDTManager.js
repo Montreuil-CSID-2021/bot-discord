74813b0298af9488b69c945ce6fb1cba2ba5d92a
Bryan Prolong <smyllet.gamer@gmail.com>
2022-03-23 21:26:58 +0100

    [CHANGE] Replace scrapping by api for edt

--- Class/EDT/EDTManager.js
+++ Class/EDT/EDTManager.js
@@ -1,5 +1,4 @@
-const { webkit} = require('playwright')
-const cheerio = require('cheerio')
+const axios = require('axios')
 
 const config = require('./../../config.json')
 const Utils = require("../Utils");
@@ -8,106 +7,33 @@ const Logs = require("../Logs");
 
 const daysOfWeek = ["Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche"]
 
-let cache = {}
-
 class EDTManager {
     /** @param {Date} dateOfWeek */
     static async scrapEDT(dateOfWeek) {
         let mondayDate = Utils.getMondayOfWeek(dateOfWeek)
+        let sundayDate = new Date(mondayDate.getTime() + 518400000)
 
-        let potentialCache = cache[mondayDate.toLocaleDateString("fr-FR")]
-        if(potentialCache && potentialCache.expire >= new Date()) {
-            Logs.info(`Récupération en cache de l'emploi du temps pour la semaine du : ${mondayDate.toLocaleDateString("fr-FR")}`)
-            return potentialCache.data
-        }
+        let response = await axios.get('http://localhost:4201/edt', {
+            headers: {
+                token: config.iut.token
+            }
+        }).catch(e => Logs.error("Impossible de récupérer l'emploi du temps" + e.toString()))
 
-        try {
-            Logs.info(`Récupération sur internet de l'emploi du temps pour la semaine du : ${mondayDate.toLocaleDateString("fr-FR")}`)
+        response = response.data
 
-            const browser = await webkit.launch()
-            const context = await browser.newContext()
-            const page = await context.newPage()
-
-            await page.goto(`https://cas.iut.univ-paris8.fr/login?service=https%3a%2f%2fent.iut.univ-paris8.fr%2f`)
-
-            await page.fill("#username", config.iut.username)
-            await page.fill("#password", config.iut.password)
-
-            await page.click("text=SE CONNECTER")
-
-            // récupération Emploi du temps
-            await page.goto(`https://ent.iut.univ-paris8.fr/edt/presentations.php`)
-
-            await page.screenshot({path: "lastCalendarView.png"})
-            await page.click("#selectsem-button")
-
-            await page.click(`#selectsem-menu>li:has-text("${mondayDate.toLocaleDateString("fr-FR")}")`)
-
-            let html = await page.content()
-
-            const $ = cheerio.load(html)
-
-            let edtHtml = $("#quadrillage").html()
-            let profList = $("#selectprof").find('option').toArray().map(el => el.children[0].data)
-
-            let edtDays = []
-
-            if(edtHtml.length > 0) {
-                edtDays = edtHtml.split("</div><div").map(el => {
-                    if(!el.startsWith("<div")) el = "<div" + el
-                    if(!el.endsWith("</div>")) el = el + "</div>"
-                    return el
-                }).filter(el => {
-                    return !el.includes("plageDIVn");
-                }).map(el => {
-                    let day = {}
-
-                    try {
-                        day.mat = el.split("<strong>")[1].split("</strong>")[0]
-                    } catch (e) {
-                        try {
-                            day.mat = el.split('<span class="plageCTRL">')[1].split("</span>")[0]
-                        } catch (e) {
-                            day.mat = "Matière inconnu"
-                        }
-                    }
-
-                    let profEtSalle = el.split(`<span class="plageHG">`)
-                    let profLettre = profEtSalle[1].split("</span>")[0]
-                    let profName = profList.find(prof => prof.includes(`[${profLettre}]`))
-                    day.prof = (profName) ? profName.toUpperCase() : (profEtSalle.length < 3) ? "Autonomie" : profLettre
-                    day.salle = profEtSalle[profEtSalle.length-1].split("</span>")[0].split("&")[0]
-
-                    let style = el.split("style=\"")[1].split("\"")[0].split(";")
-
-                    day.debut = mondayDate.getTime()/1000
-                    day.debut += 24 * 60 * 60 * style.find(s => s.includes("margin-left")).split(':')[1].split("%")[0]/80*4
-                    day.debut += 60 * (style.find(s => s.includes("top")).split(':')[1].split("px")[0]-30+480)
-
-                    day.debutText = (new Date(day.debut * 1000)).toLocaleString()
-
-                    day.fin = day.debut
-                    day.fin += 60 * (style.find(s => s.includes("height")).split(':')[1].split("px")[0])
-
-                    day.finText = (new Date(day.fin * 1000)).toLocaleString()
-
+        if(response) {
+            response.days = response.days
+                .map(day => {
+                    day.startDate = new Date(day.startDate)
+                    day.endDate = new Date(day.endDate)
                     return day
                 })
-            }
-
-            edtDays.sort((a, b) => a.debut - b.debut)
-
-            let expireCacheDate = new Date((new Date().getTime()/1000 + 3600)*1000)
-            cache[mondayDate.toLocaleDateString("fr-FR")] = {
-                expire: expireCacheDate,
-                data: edtDays
-            }
-
-            return edtDays
-        } catch (e) {
-            Logs.error(e)
-            return null
+                .filter(day => day.startDate >= mondayDate && day.startDate < sundayDate)
         }
+
+        console.log(response)
+
+        return response
     }
 
     /** @param {Date} dateOfWeek */
@@ -121,6 +47,9 @@ class EDTManager {
                 let weekDate = dateToFetch
                 let weekDateEnd = new Date(((dateToFetch.getTime()/1000)+(86400*4))*1000)
 
+                let name = edt.edt
+                let days = edt.days
+
                 let embed = new MessageEmbed()
                     .setTitle(`Semaine du ${weekDate.toLocaleDateString("fr-FR").split(",")[0]} au ${weekDateEnd.toLocaleDateString("fr-FR").split(",")[0]}`)
                     .setColor(`#0982AB`)
@@ -128,20 +57,18 @@ class EDTManager {
                         text: `Emploi du temps CSID - promo 2021/2022`
                     })
                     .setTimestamp()
-                    .setThumbnail("https://www.iut.univ-paris8.fr/sites/default/files/LOGO%20IUT%20MONTREUIL%20Moyen.jpg");
+                    .setThumbnail("https://www.iut.univ-paris8.fr/sites/default/files/inline-images/LOGO%20IUT%20MONTREUIL%20BLANC%20Moyen.png");
 
                 for(let i = 1; i<=5; i++) {
-                    let time = (dateToFetch.getTime()/1000) + (84700 * i)
-                    let dayDate = new Date(time*1000)
+                    let time = dateToFetch.getTime() + (84000000 * i)
+                    let dayDate = new Date(time)
                     let content = "Rien à signaler"
-                    if(edt) {
-                        let contentList = edt.filter(
-                            data => new Date(data.debut*1000).toLocaleDateString("fr-FR") === dayDate.toLocaleDateString("fr-FR")
+                    if(days) {
+                        let contentList = days.filter(
+                            data => data.startDate.toLocaleDateString("fr-FR") === dayDate.toLocaleDateString("fr-FR")
                         ).map(
                             data => {
-                                let dateDebut = new Date(data.debut*1000)
-                                let dateFin = new Date(data.fin*1000)
-                                return `**${data.mat}** (${data.salle})\n${data.prof}\nDe ${Utils.toTwoDigitTime(dateDebut.getHours())}:${Utils.toTwoDigitTime(dateDebut.getMinutes())} à ${Utils.toTwoDigitTime(dateFin.getHours())}:${Utils.toTwoDigitTime(dateFin.getMinutes())}`
+                                return `**${data.subject}** (${data.location})\n${data.teacher}\nDe ${Utils.toTwoDigitTime(data.startDate.getHours())}:${Utils.toTwoDigitTime(data.startDate.getMinutes())} à ${Utils.toTwoDigitTime(data.endDate.getHours())}:${Utils.toTwoDigitTime(data.endDate.getMinutes())}`
                             }
                         )
 
